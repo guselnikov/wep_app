b203888cbc77221ec2158cfdf36e2c97dd21d581
guselnikov <44221145+guselnikov@users.noreply.github.com>
2022-08-16 01:41:40 +0700

    Update settings.py

--- gusjob/gusjob/settings.py
+++ gusjob/gusjob/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g9h%8xwxl@222b&lu@+=)2pi$0a63+tmgx99zs5$#ckn5^k0^r"
+SECRET_KEY = "django-insecure-g9h%8xwxl@222b&lu@+=)2pi$0a63+tmgx99zs5$#ckn5^k0^r1a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
